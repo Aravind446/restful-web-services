a611182b400a812e4addb8930854abbcd38c45bd
Aravind446 <aravind56558@gmail.com>
2022-03-14 05:31:46 +0000

    Restful-program

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into values(1, sysdate(), 'AB');
+insert into values(2, sysdate(), 'SHIVA');
+insert into values(3, sysdate(), 'ARAVIND');
+insert into post values (11001,'My first post', 1);
+insert into post values (11002,'My second post', 2);
+insert into post values (11003,'My third post', 3);
